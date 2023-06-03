f800f1e79332d0fa6c99cc6ff89f0bd02b166800
Legend Gamerz <75073704+ANIKETEDITZ@users.noreply.github.com>
2023-06-03 12:46:20 +0530

    Update Constants.swift

--- Constants.swift
+++ Constants.swift
@@ -1,2 +1,30 @@
-client id - 14963860dd9f481c85dffca8142c9bde
-client sectet - 296d9f2157c347cf9e336b11ab344810
+
+import Foundation
+
+struct Constants {
+
+    static let clientID = "14963860dd9f481c85dffca8142c9bde"
+
+    static let clientSecret = "296d9f2157c347cf9e336b11ab344810"
+
+    static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+
+    static let redirectURI = "REDIRECT_URL" // Can be your own website url 
+
+    static let scopesArray = ["user-read-private",
+
+                               "playlist-modify-public",
+
+                               "playlist-read-private",
+
+                               "playlist-modify-private",
+
+                               "user-follow-read",
+
+                               "user-read-email"
+
+    ]
+
+//Add scopes to capture different user accesses
+
+}
